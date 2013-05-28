cbe78f1f6933475137ff1fa686cb1a0f7dbceba9
Tom Scott <tubbo@psychedeli.ca>
2013-05-27 21:28:29 -0400

    Install devise

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Bitbox
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
