bb5cd6f96e768580b16120f14e28cbdaff83660a
Tom Scott <tubbo@psychedeli.ca>
2013-05-30 00:06:36 -0400

    Build model for representing sync folders

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,3 +1,32 @@
+require 'digest'
+
 class Folder < ActiveRecord::Base
-  attr_accessible :name, :secret
+  before_validation :generate_secret
+
+  validates :name,    presence: true, uniqueness: true
+  validates :secret,  presence: true
+
+  after_validation :create_folder
+
+private
+  def generate_secret
+    self.secret ||= generator.digest source_of_secret
+  end
+
+  def create_folder
+    `mkdir #{path}` unless File.exists? path
+    true
+  end
+
+  def generator
+    @digest_generator ||= Digest::SHA256.new
+  end
+
+  def source_of_secret
+    "#{self.name} #{self.created_at}"
+  end
+
+  def path
+    "#{Rails.application.config.sync_folder}/#{name}"
+  end
 end
--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
+require 'digest'
 
 describe Folder do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:sync_folder) { Rails.application.config.sync_folder }
+  subject { Folder.new name: 'test' }
+
+  it "creates a new directory when one doesn't exist" do
+    subject.should be_valid
+    subject.secret.should be_present
+    File.exists?("#{sync_folder}/test").should be_true
+  end
+
+  it "synchronizes with an existing directory" do
+    system "mkdir #{sync_folder}/test"
+
+    subject.should be_valid
+    subject.secret.should be_present
+  end
+
+  it "synchronizes with an existing sync folder" do
+    secret_key = Digest::SHA256.new.digest "an existing secret key"
+    subject.secret = secret_key
+
+    subject.should be_valid
+    subject.secret.should == secret_key
+  end
+
+  after { system "rm -rf #{sync_folder}/test" }
 end
