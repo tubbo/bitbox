c72b471758d4420771303bce18aba65f731b16e7
Tom Scott <tubbo@psychedeli.ca>
2013-05-31 22:34:02 -0400

    Read entity information from file path

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,4 +1,24 @@
 class Entity < ActiveRecord::Base
   validates :name, presence: true
-  mount_uploader :name, EntityUploader
+
+  def file_name
+    @file_name ||= File.basename name
+  end
+
+  def path
+    @file_path ||= file.path
+  end
+
+  def size
+    @total_size ||= file.size
+  end
+
+  def last_modified_at
+    @last_modified_at ||= Date.parse file.last_modified
+  end
+
+private
+  def file
+    File.new name
+  end
 end
