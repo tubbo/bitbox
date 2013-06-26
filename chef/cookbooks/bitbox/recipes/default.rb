12861bfb3eea86f9fab4fcac9a5f192f2043e46a
Chris MacNaughton <chmacnaughton@gmail.com>
2013-06-26 14:38:27 -0400

    vagrant runs

--- chef/cookbooks/bitbox/recipes/default.rb
+++ chef/cookbooks/bitbox/recipes/default.rb
@@ -12,6 +12,12 @@ git '/var/www/bitbox' do
   repository 'git://github.com/tubbo/bitbox.git'
   action :sync
 end
+#This is so dirty!
+bash "change owner of the folder" do
+  code %(
+    sudo chown -R -f vagrant /var/www/bitbox
+  )
+end
 directory "/var/www/shared/puma" do
     recursive true
   user 'vagrant'
@@ -40,43 +46,52 @@ end
 package 'libxml2-dev' do
   action :install
 end
-gem_package 'nokogiri' do
-  action :install
-end
 package "nodejs" do # @Tom, this is for you to not use rubyracer
   action :install
 end
+gem_package 'nokogiri' do
+  action :install
+end
+
 user "bitbox" do
   action :create
   home '/home/bitbox'
 end
-postgres_user "bitbox" do
+postgresql_user "bitbox" do
   password "bitbox_user"
-  privelages \
-    superuser: true,
-    createdb: true,
-    login: true
+  privileges superuser: true, createdb: true, login: true
 end
-postgres_database "bitbox_development" do
-  owner "bitbox"
-  encoding "utf8"
-  template "template0"
-  locale "en_US.UTF8"
+["development","test"].each do |env|
+  file "/var/www/bitbox/log/#{env}.log" do
+    user "vagrant"
+    group "vagrant"
+    mode 0666
+  end
+  postgresql_database "bitbox_#{env}" do
+    owner "bitbox"
+    encoding "utf8"
+    template "template0"
+    locale "en_US.UTF8"
+  end
 end
-postgres_database "bitbox_test" do
-  owner "bitbox"
-  encoding "utf8"
-  template "template0"
-  locale "en_US.UTF8"
+file '/var/www/bitbox/tmp/ember-rails/ember.js' do
+  user "vagrant"
+  group "vagrant"
+  mode 0666
 end
 bash "install gems" do
   code %(
     cd /var/www/bitbox &&
     bundle install --deployment --without=development,test
-    rake db:migrate
   )
 end
-
+bash "setup DB" do
+  code %(
+    cd /var/www/bitbox &&
+    bundle exec rake db:migrate &&
+    bundle exec rake db:test:prepare
+  )
+end
 puma_config 'bitbox' do
   @user = node[:puma][:user]
   environment node[:rails][:environment]
--- chef/roles/bitbox.rb
+++ chef/roles/bitbox.rb
@@ -3,18 +3,18 @@ description "Provisions and installs the latest version of Bitbox"
 run_list [
   'recipe[build-essential]',
   'recipe[apt]',
-  "recipe[postgresql]",
+  'recipe[postgresql::server]',
   'recipe[btsync]',
   'recipe[bitbox]'
 ]
 default_attributes \
   postgresql: {
-    username: 'bitbox',
+    username: 'postgres',
     superuser: true,
     createdb: true,
     login: true,
     pg_hba: [
-      {type: 'local', db: 'all', user: 'all', addr: nil, method: 'trust'}
+      {type: 'host', db: 'all', user: 'bitbox', addr: nil, method: 'md5'}
     ],
     password: {
       postgres: '9678fd32f829d44e4eb45b0916b5adbc'
@@ -23,7 +23,7 @@ default_attributes \
   puma: {
     socket_url: 'unix:///var/www/bitbox/tmp/puma.sock',
     rubygems_location: '/usr/bin/gem',
-    user: "vagrant",
+    user: 'vagrant',
     bind_address: 'tcp://127.0.0.1:3000'
   },
   rails: {
